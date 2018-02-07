c53ff32a3e7b98e3d4b0f8c7f6cb3a67ede0553d
jsmith <jasmith79@gmail.com>
2018-02-07 11:46:03 -0500

    Bumped deps, added tests

--- spec/auto/spec.js
+++ spec/auto/spec.js
@@ -1,6 +1,18 @@
+import API_KEY from '../support/apikey.js';
+import { GoogleMap } from '../../src/gmap.js';
+
+window.alert = () => {};
+
+const timeout2Promise = (n, f, ...args) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(f(...args));
+    }, n);
+  });
+};
+
 describe('ui-google-map', () => {
   let div;
-  let API_KEY = window.GOOGLE_MAPS_API_KEY;
 
   beforeEach(() => {
     div = document.createElement('div');
@@ -9,52 +21,185 @@ describe('ui-google-map', () => {
   });
 
   afterEach(() => {
-    let els = document.querySelectorAll('.remove-me');
-    els.forEach(x => document.removeChild(x));
+    let els = [...document.querySelectorAll('.remove-me')];
+    els.forEach(x => document.body.removeChild(x));
   });
 
-  it('should be constructable by being added to the innerHTML of another element', () => {
+  it('should be constructable by being added to the innerHTML of another element', done => {
     div.innerHTML += `<ui-google-map api-key="${API_KEY}"></ui-google-map>`;
-    let m = div.querySelector('ui-google-map');
+    let map = div.querySelector('ui-google-map');
     map.onReady(_ => {
       expect(google).toBeDefined();
       expect(google.maps).toBeDefined();
-      done();
-    });
-  });
-
-  it('should be constructable via document.createElement', done => {
-    let m = document.createElement('ui-google-map');
-    m.attr('api-key', API_KEY);
-    m.onReady(_ => {
-      expect(true).toBe(true);
-      done();
-    });
-    div.appendChild(m);
-  });
-
-  it('should have a map property that is a google.maps.Map', done => {
-    div.innerHTML += `<ui-google-map api-key="${API_KEY}"></ui-google-map>`;
-    let m = div.querySelector('ui-google-map');
-    map.onReady(_ => {
       expect(map.map instanceof google.maps.Map).toBe(true);
       done();
+    }).catch(err => {
+      console.error(err);
+      expect(false).toBe(true);
+      done();
     });
   });
 
-  it('should throw on no API key or the placeholder', done => {
+  // Because of the inherent need for an API key in the early stages of construction, using
+  // document.createElement won't work. Once it's possible to construct the element via `new`
+  // there's a constructor parameter for taking the API key but that hasn't currently been
+  // implemented in any browser AFAIK.
+  it('should NOT be constructable via document.createElement', done => {
+    let handle = setTimeout(() => {
+      expect(true).toBe(false);
+      window.onerror = null;
+      done();
+    }, 150);
 
+    let listener = err => {
+      clearTimeout(handle);
+      console.log(err);
+      expect(true).toBe(true);
+      window.onerror = null;
+      done();
+    };
+
+    window.onerror = listener;
+    let map = document.createElement('ui-google-map');
   });
 
   it('should reflect latitude, longitude, zoom', done => {
+    div.innerHTML += `<ui-google-map api-key="${API_KEY}"></ui-google-map>`;
+    let map = div.querySelector('ui-google-map');
+    map.onReady(_ => {
+      expect(map.latitude).toBeDefined();
+      expect(typeof map.latitude).toBe('number');
 
+      expect(map.longitude).toBeDefined();
+      expect(typeof map.longitude).toBe('number');
+
+      expect(map.zoomLevel).toBeDefined();
+      expect(typeof map.zoomLevel).toBe('number');
+
+      expect(map.attr('latitude')).not.toBeNull();
+      expect(map.attr('latitude') > 0).toBe(true);
+      expect(Number.isNaN(+map.attr('latitude'))).toBe(false);
+
+      expect(map.attr('longitude')).not.toBeNull();
+      expect(map.attr('longitude') < 0).toBe(true);
+      expect(Number.isNaN(+map.attr('longitude'))).toBe(false);
+
+      expect(map.attr('zoom-level')).not.toBeNull();
+      expect(map.attr('zoom-level') > 0).toBe(true);
+      expect(Number.isNaN(+map.attr('zoom-level'))).toBe(false);
+
+      done();
+    }).catch(err => {
+      console.error(err);
+      expect(false).toBe(true);
+      done();
+    });
   });
 
+  // timeouts allow animation to finish
   it('should pan on arrow keys when the map is hover', done => {
+    div.innerHTML += `<ui-google-map api-key="${API_KEY}"></ui-google-map>`;
+    let map = div.querySelector('ui-google-map');
+    map.onReady(_ => {
+      map._addKeyboardHandler();
+      let oldCenter = map.map.getCenter(), newCenter, evt;
 
+      evt = new KeyboardEvent('keydown', { key: 'ArrowDown' });
+      document.dispatchEvent(evt);
+      timeout2Promise(5, () => {
+        newCenter = map.map.getCenter();
+        expect(newCenter.toString()).not.toBe(oldCenter.toString());
+        oldCenter = newCenter;
+
+        evt = new KeyboardEvent('keydown', { key: 'ArrowUp' });
+        document.dispatchEvent(evt);
+      }).then(_ => {
+        return timeout2Promise(5, () => {
+          newCenter = map.map.getCenter();
+          expect(newCenter.toString()).not.toBe(oldCenter.toString());
+          oldCenter = newCenter;
+        });
+      }).then(_ => {
+        evt = new KeyboardEvent('keydown', { key: 'ArrowUp' });
+        document.dispatchEvent(evt);
+      }).then(_ => {
+        return timeout2Promise(5, () => {
+          newCenter = map.map.getCenter();
+          expect(newCenter.toString()).not.toBe(oldCenter.toString());
+          oldCenter = newCenter;
+        });
+      }).then(_ => {
+        evt = new KeyboardEvent('keydown', { key: 'ArrowRight' });
+        document.dispatchEvent(evt);
+      }).then(_ => {
+        return timeout2Promise(5, () => {
+          newCenter = map.map.getCenter();
+          expect(newCenter.toString()).not.toBe(oldCenter.toString());
+          oldCenter = newCenter;
+        });
+      }).then(_ => {
+        evt = new KeyboardEvent('keydown', { key: 'ArrowLeft' });
+        document.dispatchEvent(evt);
+      }).then(_ => {
+        newCenter = map.map.getCenter();
+        expect(newCenter.toString()).not.toBe(oldCenter.toString());
+        done();
+      }).catch(err => {
+        console.error(err);
+        expect(false).toBe(true);
+        done();
+      });
+    }).catch(err => {
+      console.error(err);
+      expect(false).toBe(true);
+      done();
+    });
   });
 
   it('should zoom on +/= and - when map is hover', done => {
+    div.innerHTML += `<ui-google-map api-key="${API_KEY}"></ui-google-map>`;
+    let map = div.querySelector('ui-google-map');
+    map.onReady(_ => {
+      map._addKeyboardHandler();
+      let oldZoom = map.map.getZoom(), newZoom, evt;
+      evt = new KeyboardEvent('keydown', { key: 'Minus' });
+      document.dispatchEvent(evt);
 
+      timeout2Promise(5, () => {
+        newZoom = map.map.getZoom();
+        expect(newZoom.toString()).not.toBe(oldZoom.toString());
+        oldZoom = newZoom;
+      }).then(_ => {
+        evt = new KeyboardEvent('keydown', { key: 'Equal' });
+        document.dispatchEvent(evt);
+      }).then(_ => {
+        return timeout2Promise(5, () => {
+          newZoom = map.map.getZoom();
+          expect(newZoom.toString()).not.toBe(oldZoom.toString());
+          oldZoom = newZoom;
+        });
+      }).then(_ => {
+        evt = new KeyboardEvent('keydown', { key: 'Subtract' });
+        document.dispatchEvent(evt);
+      }).then(_ => {
+        return timeout2Promise(5, () => {
+          newZoom = map.map.getZoom();
+          expect(newZoom.toString()).not.toBe(oldZoom.toString());
+          oldZoom = newZoom;
+        });
+      }).then(_ => {
+        evt = new KeyboardEvent('keydown', { key: 'Add' });
+        document.dispatchEvent(evt);
+        done();
+      }).catch(err => {
+        console.error(err);
+        expect(false).toBe(true);
+        done();
+      });
+    }).catch(err => {
+      console.error(err);
+      expect(false).toBe(true);
+      done();
+    });
   });
-;});
+});
--- src/gmap.js
+++ src/gmap.js
@@ -3,7 +3,7 @@ import {
   defineUIComponent,
   document,
   global
-} from '../../ui-components-lite/src/utils/ui-component-base.js';
+} from '../node_modules/ui-components-lite/src/utils/ui-component-base.js';
 
 const tooLongMessage = `Sorry but google maps is taking longer than expected to load.
   If this message does not disappear in the next couple of seconds please refresh the page.`;
@@ -12,6 +12,38 @@ const failMessage = `Oops! Looks like Google Maps failed to load. Please refresh
 
 const warnTimeout = 8000;
 const failTimeout = 15000;
+
+// One of the most idiotic things I've ever seen, and for JavaScript, that's really saying
+// something.
+const KEYMAP = key => {
+  switch (key) {
+    case 'ArrowUp':
+    case 'Up':
+      return 38;
+
+    case 'ArrowDown':
+    case 'Down':
+      return 40;
+
+    case 'ArrowLeft':
+    case 'Left':
+      return 37;
+
+    case 'ArrowRight':
+    case 'Right':
+      return 39;
+
+    case 'Add':
+    case 'Equal':
+      return 187;
+
+    case 'Minus':
+    case 'Subtract':
+      return 189;
+
+    default: return 0;
+  }
+};
 
 let mapsLoaded = false;
 
@@ -59,7 +91,7 @@ template.innerHTML = `
   <div id="map-container"></div>
 `;
 
-const reflectedAttrs = [
+const reflectedAttributes = [
   'latitude',
   'longitude',
   'map-type',
@@ -69,12 +101,15 @@ const reflectedAttrs = [
 export const GoogleMap = defineUIComponent({
   name: 'ui-google-map',
   template,
+  reflectedAttributes,
   definition: class GoogleMap extends UIBase {
-    constructor () {
+    constructor (apikey) {
       super();
       this._mapContainer = null;
       this._mapObj = null;
       this._center = null;
+      this._keydownHandler = null;
+      this._API_KEY = apikey;
     }
 
     get map () {
@@ -94,13 +129,24 @@ export const GoogleMap = defineUIComponent({
       return this;
     }
 
+    _addKeyboardHandler () {
+      document.addEventListener('keydown', this._keydownHandler);
+      return this;
+    }
+
+    _removeKeyboardHandler () {
+      document.removeEventListener('keydown', this._keydownHandler);
+      return this;
+    }
+
     init () {
       super.init();
 
+      const API_KEY = this.attr('api-key') || this._API_KEY;
+      if (!API_KEY || API_KEY === 'YOUR_API_KEY_HERE') throw new Error('No google maps API key!');
+
       if (!mapsLoaded) {
         mapsLoaded = true;
-        const API_KEY = this.attr('api-key');
-        if (!API_KEY || API_KEY === 'YOUR_API_KEY_HERE') throw new Error('No google maps API key!');
         const mapsAPI = document.createElement('script');
         mapsAPI.src = 'https://maps.googleapis.com/maps/api/js?' +
           `key=${API_KEY}&callback=_resolveMapsLoader`;
@@ -108,85 +154,95 @@ export const GoogleMap = defineUIComponent({
         document.head.appendChild(mapsAPI);
       }
 
-      mapsAPILoaded.then(_ => {
-        this._mapContainer = this.selectInternalElement('#map-container');
-        const options = {
-          zoom: 11,
-          center: new global.google.maps.LatLng(39.75, -86.16),
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+      this._beforeReady(() => {
+        return mapsAPILoaded.then(_ => {
+          this._mapContainer = this.selectInternalElement('#map-container');
+          const options = {
+            zoom: 11,
+            center: new global.google.maps.LatLng(39.75, -86.16),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
 
-        if (this.mapType && this.mapType.toUpperCase() in global.google.maps.MapTypeId) {
-          options.mapTypeId = global.google.maps[this.mapType.toUpperCase()];
-        }
-
-        if (this.zoomLevel) options.zoom = this.zoomLevel;
-        if (this.latitude && this.longitude) options.center = new global.google.maps.LatLng(
-          this.latitude,
-          this.longitude
-        );
-
-        this._center = options.center;
-        this._mapObj = new global.google.maps.Map(this._mapContainer, options);
-
-        // Google maps really don't like to be unrendered, especially when they're first created:
-        // tiles won't render until a resize event is triggered on window. So we'll track center
-        // changes and set it manually. Currently the parent element responsible for rendering
-        // this element needs to trigger resize on visibility changes, which I'm hoping to fix.
-        // Currently, if setting the center programmatically, use the element's method rather
-        // than the underlying map object's.
-        this._mapObj.addListener('zoom_changed', e => {
-          this._center = this.map.getCenter();
-        });
-
-        this._mapObj.addListener('dragend', e => {
-          this._center = this.map.getCenter();
-        });
-
-        // mimic the google maps desktop keyboard shortcuts
-        let keydownHandler = e => {
-          switch (e.keyCode) {
-            case 107: // numpad add
-            case 187: // equals/plus
-              this._mapObj.setZoom(this._mapObj.getZoom() + 1);
-              break;
-
-            case 109: // numpad subtract
-            case 189: // minus
-              this._mapObj.setZoom(this._mapObj.getZoom() - 1);
-              break;
-
-            case 37: // left
-              this._mapObj.panBy(-50, 0);
-              break;
-
-            case 38: // up
-              this._mapObj.panBy(0, 50);
-              break;
-
-            case 39: // right
-              this._mapObj.panBy(50, 0);
-              break;
-
-            case 40: // down
-              this._mapObj.panBy(0, -50);
-              break;
-
+          if (this.mapType && this.mapType.toUpperCase() in global.google.maps.MapTypeId) {
+            options.mapTypeId = global.google.maps[this.mapType.toUpperCase()];
           }
-        };
 
-        this.on('mouseenter', e => {
-          document.addEventListener('keydown', keydownHandler);
-        });
+          if (this.attr('zoom-level')) {
+            options.zoom = this.attr('zoom-level');
+          } else {
+            this.attr('zoom-level', options.zoom);
+          }
 
-        this.on('mouseleave', e => {
-          document.removeEventListener('keydown', keydownHandler);
-        });
+          if (this.attr('latitude') && this.attr('longitude')) {
+            options.center = new global.google.maps.LatLng(this.attr('latitude'), this.attr('longitude'));
+          } else {
+            this.attr('latitude', options.center.lat());
+            this.attr('longitude', options.center.lng());
+          }
 
-        global.addEventListener('resize', e => {
-          global.setTimeout(() => {
-            this.map.setCenter(this._center);
-          }, 0);
+          this._center = options.center;
+          this._mapObj = new global.google.maps.Map(this._mapContainer, options);
+
+          // Google maps really don't like to be unrendered, especially when they're first created:
+          // tiles won't render until a resize event is triggered on window. So we'll track center
+          // changes and set it manually. Currently the parent element responsible for rendering
+          // this element needs to trigger resize on visibility changes, which I'm hoping to fix.
+          // Currently, if setting the center programmatically, use the element's method rather
+          // than the underlying map object's.
+          this._mapObj.addListener('zoom_changed', e => {
+            this._center = this.map.getCenter();
+          });
+
+          this._mapObj.addListener('dragend', e => {
+            this._center = this.map.getCenter();
+          });
+
+          // mimic the google maps desktop keyboard shortcuts
+          this._keydownHandler = e => {
+            let value = e.keyCode || KEYMAP(e.key);
+            switch (value) {
+              case 107: // numpad add
+              case 187: // equals/plus
+                this._mapObj.setZoom(this._mapObj.getZoom() + 1);
+                break;
+
+              case 109: // numpad subtract
+              case 189: // minus
+                this._mapObj.setZoom(this._mapObj.getZoom() - 1);
+                break;
+
+              case 37: // left
+                this._mapObj.panBy(-50, 0);
+                break;
+
+              case 38: // up
+                this._mapObj.panBy(0, 50);
+                break;
+
+              case 39: // right
+                this._mapObj.panBy(50, 0);
+                break;
+
+              case 40: // down
+                this._mapObj.panBy(0, -50);
+                break;
+
+            }
+          };
+
+          this.on('mouseenter', e => {
+            this._addKeyboardHandler();
+          });
+
+          this.on('mouseleave', e => {
+            this._removeKeyboardHandler();
+          });
+
+          global.addEventListener('resize', e => {
+            global.setTimeout(() => {
+              this.map.setCenter(this._center);
+            }, 0);
+          });
         });
       });
     }
